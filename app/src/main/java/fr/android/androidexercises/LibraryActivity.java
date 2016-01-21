eb5ecdbee3c662201b0831632ecaef23531de10f
theophileMorin <theophile.morin.pro@gmail.com>
2016-01-21 14:52:48 +0100

    ** Feat **: done

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -1,5 +1,7 @@
 package fr.android.androidexercises;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -19,6 +24,8 @@ public class LibraryActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Button openButton = (Button) findViewById(R.id.openButton);
+        Button dateButton = (Button) findViewById(R.id.dateButton);
+        Button hourButton = (Button) findViewById(R.id.hourButton);
 
         final Book book = new Book("Garry Whopper", "CK Rowling");
 
@@ -29,6 +36,30 @@ public class LibraryActivity extends AppCompatActivity {
                 // Add book to intent
                 intent.putExtra("BOOK", book);
                 startActivity(intent);
+            }
+        });
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               new DatePickerDialog(LibraryActivity.this, new DatePickerDialog.OnDateSetListener() {
+                   @Override
+                   public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                       Toast.makeText(LibraryActivity.this, dayOfMonth+ "/" + monthOfYear +"/" + year, Toast.LENGTH_LONG ).show();
+                   }
+               }, 2016, 0, 21).show();
+            }
+        });
+
+        hourButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(LibraryActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Toast.makeText(LibraryActivity.this, hourOfDay +":" + minute, Toast.LENGTH_LONG).show();
+                    }
+                }, 12, 12, true).show();
             }
         });
     }
