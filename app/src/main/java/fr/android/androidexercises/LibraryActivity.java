16770aa537bbd45e02f7f6b21fbe0f0d60403572
theophileMorin <theophile.morin.pro@gmail.com>
2016-01-21 16:36:23 +0100

    ** Feat **: done

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,6 +2,9 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -12,6 +15,11 @@ public class LibraryActivity extends AppCompatActivity {
 
         // TODO Get image view and load image form URL
         // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
+        ImageView dllImg = (ImageView) findViewById(R.id.downloadImageView);
+
+        Picasso.with(this)
+                .load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg")
+                .into(dllImg);
     }
 
 }
