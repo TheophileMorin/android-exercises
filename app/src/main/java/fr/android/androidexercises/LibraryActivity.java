d002650108f1928d27cbb2366aa2d8cdd4ee4377
theophileMorin <theophile.morin.pro@gmail.com>
2016-01-22 11:11:48 +0100

    ** Feat **: fragment used

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -1,20 +1,49 @@
 package fr.android.androidexercises;
 
+import android.app.FragmentManager;
+import android.content.res.Configuration;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class LibraryActivity extends AppCompatActivity implements Step0Fragment.OnNextStep0Listener {
+public class LibraryActivity extends AppCompatActivity implements Step0Fragment.OnNextStep0Listener, Step1Fragment.OnNextStep1Listener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
-        // TODO replace Step0Fragment in containerFrameLayout
+
+        boolean landscape = getResources().getBoolean(R.bool.landscape); //getResources().getBoolean(R.bool.landscape);
+        if(landscape) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment0, new Step0Fragment(), Step0Fragment.class.getSimpleName())
+                    .commit();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment1, new Step1Fragment(), Step1Fragment.class.getSimpleName())
+                    .commit();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment0, new Step0Fragment(), Step0Fragment.class.getSimpleName())
+                    .commit();
+            findViewById(R.id.fragment1).setVisibility(View.GONE); //Eviter la rémanance
+        }
+    }
+
+
+    @Override
+    public void on0Next() {
+        // TODO replace Step1Fragment in containerFrameLayout
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment0, new Step1Fragment(), Step1Fragment.class.getSimpleName())
+                .addToBackStack(Step0Fragment.class.getSimpleName())
+                .commit();
     }
 
     @Override
-    public void onNext() {
-        // TODO replace Step1Fragment in containerFrameLayout
+    public void on1Next() {
+
     }
 
 }
--- app/src/main/java/fr/android/androidexercises/Step1Fragment.java
+++ app/src/main/java/fr/android/androidexercises/Step1Fragment.java
@@ -1,9 +1,43 @@
 package fr.android.androidexercises;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Step1Fragment extends Fragment {
 
-    // TODO Override onCreateViewMethod
+    private static final String step1 = "This is step 1";
+
+    private TextView textView;
+    private OnNextStep1Listener listener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        listener = (OnNextStep1Listener) context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_step1, container, false);
+        textView = (TextView) view.findViewById(R.id.step1TextView);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        textView.setText(step1);
+    }
+
+    public interface OnNextStep1Listener {
+        void on1Next();
+    }
 
 }
